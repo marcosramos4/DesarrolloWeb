e7e2acb57552f522aef76b058c0b058e0f97da37
JhonRams <64045380+JhonRams@users.noreply.github.com>
2020-06-30 11:32:39 -0500

    Update ejerciciofuncion.php

--- ejerciciofuncion.php
+++ ejerciciofuncion.php
@@ -6,7 +6,7 @@ function espar_o_impar($n){
         echo "numero impar<br>";
     }
 }
-echo"<br>";
+echo"1-Crear una funcion que muestre si un numero es par o impar<br>";
 espar_o_impar(5);
 function mayor_numero($n1,$n2){
     if($n1>$n2){
@@ -15,13 +15,13 @@ function mayor_numero($n1,$n2){
           echo "El mayor es:$n2<br>";
     }
 }
-echo"<br>";
+echo"2-Crear una funcion que determine el mayor de dos numeros<br>";
 mayor_numero(5, 2);
 function cuadrado_numero($n){
     $cuadrado=$n*$n;
-    echo "el numero al cuadrado es: $cuadrado";
+    echo "el numero al cuadrado es: $cuadrado<br>";
 }
-echo"<br>";
+echo"3-Crear una funcion para calcular el cuadrado de un numero <br>";
 cuadrado_numero(5);
 
 ?>
